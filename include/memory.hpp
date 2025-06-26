f16bd9f7ce469e74b2b1e031699e40c5dc0d79a7
louisb0 <louisbaragwanath@outlook.com>
2025-06-27 08:07:49 +1200

    Implement uninitialized_copy()

---
+++ include/memory.hpp
@@ -0,0 +1,23 @@
+#include "iterator.hpp"
+
+#include <memory>
+
+namespace mystd {
+
+template <input_iterator InputIt, forward_iterator OutputIt>
+OutputIt uninitialized_copy(InputIt first, InputIt last, OutputIt result) {
+    using T = iterator_traits<OutputIt>::value_type;
+
+    OutputIt current = result;
+    try {
+        for (; first != last; ++first, ++current)
+            ::new (static_cast<void *>(std::addressof(*current))) T(*first);
+        return current;
+    } catch (...) {
+        for (; result != current; ++result)
+            (*result).~T();
+        throw;
+    }
+}
+
+} // namespace mystd
---
+++ tests/test_memory.cpp
@@ -0,0 +1,55 @@
+#include "memory.hpp"
+
+#include <gtest/gtest.h>
+#include <string>
+
+TEST(Memory, UninitializedCopyPODType) {
+    alignas(int) std::byte buffer[sizeof(int) * 3];
+    int src[3] = {1, 2, 3};
+    int *dest = reinterpret_cast<int *>(buffer);
+
+    auto it = mystd::uninitialized_copy(src, src + 3, dest);
+    EXPECT_EQ(it, dest + 3);
+
+    for (int i = 0; i < 3; i++) {
+        EXPECT_EQ(dest[i], src[i]);
+    }
+}
+
+TEST(Memory, UninitializedCopyClassType) {
+    alignas(std::string) std::byte buffer[sizeof(std::string) * 3];
+    std::string src[3] = {"hello", "world", "test"};
+    std::string *dest = reinterpret_cast<std::string *>(buffer);
+
+    auto it = mystd::uninitialized_copy(src, src + 3, dest);
+    EXPECT_EQ(it, dest + 3);
+
+    for (int i = 0; i < 3; i++) {
+        EXPECT_EQ(dest[i], src[i]);
+    }
+}
+
+struct ThrowOnThirdCopy {
+    static inline size_t copy_count = 0;
+
+    ThrowOnThirdCopy() = default;
+    ThrowOnThirdCopy(const ThrowOnThirdCopy &other) {
+        if (copy_count + 1 == 3) {
+            throw std::runtime_error("third copy");
+        }
+
+        copy_count++;
+    }
+
+    ~ThrowOnThirdCopy() { copy_count--; }
+};
+
+TEST(Memory, UninitializedCopyThrows) {
+    alignas(ThrowOnThirdCopy) std::byte buffer[sizeof(ThrowOnThirdCopy) * 3];
+    ThrowOnThirdCopy src[3];
+    ThrowOnThirdCopy *dest = reinterpret_cast<ThrowOnThirdCopy *>(buffer);
+
+    EXPECT_EQ(ThrowOnThirdCopy::copy_count, 0);
+    EXPECT_THROW({ mystd::uninitialized_copy(src, src + 3, dest); }, std::runtime_error);
+    EXPECT_EQ(ThrowOnThirdCopy::copy_count, 0);
+}
