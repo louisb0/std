bf3b5889cde770098fb74f44fb3dbcbe96e9e1ed
louisb0 <louisbaragwanath@outlook.com>
2025-07-04 10:52:08 +1200

    Add move_backward algorithm

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -5,12 +5,21 @@
 namespace mystd {
 
 template <input_iterator I, output_iterator<typename iterator_traits<I>::value_type &&> O>
-O move(I first, I last, O result) {
-    for (; first != last; ++first, ++result) {
-        *result = std::move(*first);
+O move(I first, I last, O d_first) {
+    for (; first != last; ++first, ++d_first) {
+        *d_first = std::move(*first);
     }
 
-    return result;
+    return d_first;
+}
+
+template <bidirectional_iterator I, bidirectional_iterator O>
+O move_backward(I first, I last, O d_last) {
+    while (first != last) {
+        *(--d_last) = std::move(*--last);
+    }
+
+    return d_last;
 }
 
 } // namespace mystd
--- tests/test_algorithm.cpp
+++ tests/test_algorithm.cpp
@@ -2,21 +2,34 @@
 
 #include <gtest/gtest.h>
 
+struct Moveable {
+    bool moved_from = false;
+
+    Moveable() = default;
+    Moveable &operator=(Moveable &&other) {
+        other.moved_from = true;
+        return *this;
+    }
+};
+
 TEST(Algorithm, Move) {
-    struct Moveable {
-        bool moved_from = false;
-
-        Moveable() = default;
-        Moveable &operator=(Moveable &&other) {
-            other.moved_from = true;
-            return *this;
-        }
-    };
-
     Moveable src[5];
     Moveable dest[5];
 
     auto it = mystd::move(src, src + 4, dest);
+    EXPECT_FALSE(it->moved_from);
+
+    for (int i = 0; i < 4; i++) {
+        EXPECT_TRUE(src[i].moved_from);
+        EXPECT_FALSE(dest[i].moved_from);
+    }
+}
+
+TEST(Algorithm, Backward) {
+    Moveable src[5];
+    Moveable dest[5];
+
+    auto it = mystd::move_backward(src, src + 4, dest + 4);
     EXPECT_FALSE(it->moved_from);
 
     for (int i = 0; i < 4; i++) {
