53eb48ba3cf662e2c081adb641b37ef198c7fbde
louisb0 <louisbaragwanath@outlook.com>
2025-07-21 17:26:47 +1200

    Implement mystd::next() and mystd::advance()

--- include/bits/iterator_functions.hpp
+++ include/bits/iterator_functions.hpp
@@ -22,4 +22,31 @@ typename mystd::iterator_traits<I>::difference_type distance(I first, I last) {
     }
 }
 
+template <mystd::input_iterator I>
+void advance(I &it, typename mystd::iterator_traits<I>::difference_type n) {
+    using category = typename mystd::iterator_traits<I>::iterator_category;
+
+    if constexpr (std::derived_from<category, mystd::random_access_iterator_tag>) {
+        it += n;
+    } else {
+        while (n > 0) {
+            --n;
+            ++it;
+        }
+
+        if constexpr (std::derived_from<category, mystd::bidirectional_iterator_tag>) {
+            while (n < 0) {
+                ++n;
+                --it;
+            }
+        }
+    }
+}
+
+template <mystd::input_iterator I>
+I next(I it, typename mystd::iterator_traits<I>::difference_type n = 1) {
+    advance(it, 1);
+    return it;
+}
+
 } // namespace mystd
--- tests/iterator/test_functions.cpp
+++ tests/iterator/test_functions.cpp
@@ -1,37 +1,107 @@
+#include "bits/iterator_functions.hpp"
 #include "iterator.hpp"
+#include "vector.hpp"
 
 #include <gtest/gtest.h>
 
 constexpr size_t count = 3;
 constexpr int data[] = {1, 2, 3};
 
-TEST(IteratorFunctions, DistanceRandomAccess) {
+class InputIterator : public mystd::iterator<mystd::input_iterator_tag, int> {
+    size_t pos_;
+
+public:
+    explicit InputIterator(size_t pos = 0) : pos_(pos) {}
+
+    InputIterator &operator++() {
+        ++pos_;
+        return *this;
+    }
+    InputIterator operator++(int) { return InputIterator(pos_++); }
+
+    value_type operator*() const { return data[pos_]; }
+
+    bool operator==(const InputIterator &other) const { return pos_ == other.pos_; }
+};
+
+class BidirectionalIterator : public mystd::iterator<mystd::bidirectional_iterator_tag, int> {
+    size_t pos_;
+
+public:
+    explicit BidirectionalIterator(size_t pos = 0) : pos_(pos) {}
+
+    BidirectionalIterator &operator++() {
+        ++pos_;
+        return *this;
+    }
+    BidirectionalIterator operator++(int) { return BidirectionalIterator(pos_++); }
+
+    BidirectionalIterator &operator--() {
+        --pos_;
+        return *this;
+    }
+    BidirectionalIterator operator--(int) { return BidirectionalIterator(pos_--); }
+
+    value_type operator*() const { return data[pos_]; }
+
+    bool operator==(const BidirectionalIterator &other) const { return pos_ == other.pos_; }
+};
+
+TEST(IteratorFunctions, DistanceRandomAccessIterator) {
     EXPECT_EQ(mystd::distance(data, data + count), count);
 }
 
-TEST(IteratorFunctions, DistanceIncrementOnly) {
-    class IncrementOnly : public mystd::iterator<mystd::input_iterator_tag, int> {
-        size_t pos_;
+TEST(IteratorFunctions, DistanceInputIterator) {
+    EXPECT_TRUE(mystd::input_iterator<InputIterator>);
 
-    public:
-        explicit IncrementOnly(size_t pos = 0) : pos_(pos) {}
-
-        IncrementOnly &operator++() {
-            ++pos_;
-            return *this;
-        }
-        IncrementOnly operator++(int) { return IncrementOnly(pos_++); }
-
-        value_type operator*() const { return data[pos_]; }
-
-        bool operator==(const IncrementOnly &other) const { return pos_ == other.pos_; }
-    };
-
-    static_assert(mystd::input_iterator<IncrementOnly>);
-
-    IncrementOnly begin(0);
-    IncrementOnly end(count);
+    InputIterator begin(0);
+    InputIterator end(count);
 
     EXPECT_EQ(mystd::distance(begin, end), count);
     EXPECT_EQ(mystd::distance(begin, begin), 0);
+}
+
+TEST(IteratorFunctions, AdvanceInputOnlyIterator) {
+    EXPECT_TRUE(mystd::input_iterator<InputIterator>);
+
+    InputIterator it(0);
+
+    mystd::advance(it, 0);
+    EXPECT_EQ(*it, 1);
+
+    mystd::advance(it, 2);
+    EXPECT_EQ(*it, 3);
+
+    InputIterator it2(0);
+    mystd::advance(it2, 0);
+    EXPECT_EQ(*it2, 1);
+}
+
+TEST(IteratorFunctions, AdvanceBidirectional) {
+    EXPECT_TRUE(mystd::bidirectional_iterator<BidirectionalIterator>);
+
+    BidirectionalIterator it(0);
+
+    mystd::advance(it, 0);
+    EXPECT_EQ(*it, 1);
+
+    mystd::advance(it, 2);
+    EXPECT_EQ(*it, 3);
+
+    mystd::advance(it, -2);
+    EXPECT_EQ(*it, 1);
+}
+
+TEST(IteratorFunctions, AdvanceRandomAccessIterator) {
+    auto it = data;
+    EXPECT_TRUE(mystd::random_access_iterator<decltype(it)>);
+
+    mystd::advance(it, 0);
+    EXPECT_EQ(*it, 1);
+
+    mystd::advance(it, 2);
+    EXPECT_EQ(*it, 3);
+
+    mystd::advance(it, -2);
+    EXPECT_EQ(*it, 1);
 }
